45dbde6b64daf5922c864543da5dd3414c55ee9b
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-10-22 20:22:51 -0300

    tela consultas

--- src/utils/splash_screen.py
+++ src/utils/splash_screen.py
@@ -3,84 +3,79 @@ from src.conexion.conexao_oracle import ConexaoOracle
 from src.utils import config
 
 class SplashScreen:
-
     def __init__(self):
-        # Consultas de contagem de registros - inicio
         self.qry_total_fornecedores = config.QUERY_COUNT.format(tabela="fornecedores")
         self.qry_total_funcionarios = config.QUERY_COUNT.format(tabela="funcionarios")
         self.qry_total_movimentacoes_estoque = config.QUERY_COUNT.format(tabela="movimentacao_estoque")
         self.qry_total_produtos_fornecedores = config.QUERY_COUNT.format(tabela="produtos_fornecedores")
         self.qry_total_produtos = config.QUERY_COUNT.format(tabela="produtos")
-        # Consultas de contagem de registros - fim
-
-        # Nome(s) do(s) criador(es)
-        # Nome(s) do(s) criador(es)
-        self.created_by = '''Arthur Pomarolli, 
-                                #              Davi de Souza, 
-                                #              Mauro Barros, 
-                                #              Pedro Augusto.'''
-        self.professor = "Prof. M.Sc. Howard Roatti."
-        self.disciplina = "Banco de dados"
-        self.semestre = "2025/2"
 
     def get_total_fornecedores(self):
-        # Cria uma nova conexão com o banco que permite alteração
         oracle = ConexaoOracle()
         oracle.connect()
-        # Retorna o total de registros computado pela query
         return oracle.sqlToDataFrame(self.qry_total_fornecedores)["total_fornecedores"].values[0]
 
     def get_total_funcionarios(self):
-        # Cria uma nova conexão com o banco que permite alteração
         oracle = ConexaoOracle()
         oracle.connect()
-        # Retorna o total de registros computado pela query
         return oracle.sqlToDataFrame(self.qry_total_funcionarios)["total_funcionarios"].values[0]
 
     def get_total_movimentacoes_estoque(self):
-        # Cria uma nova conexão com o banco que permite alteração
         oracle = ConexaoOracle()
         oracle.connect()
-        # Retorna o total de registros computado pela query
         return oracle.sqlToDataFrame(self.qry_total_movimentacoes_estoque)["total_movimentacao_estoque"].values[0]
 
     def get_total_produtos_fornecedores(self):
-        # Cria uma nova conexão com o banco que permite alteração
         oracle = ConexaoOracle()
         oracle.connect()
-        # Retorna o total de registros computado pela query
         return oracle.sqlToDataFrame(self.qry_total_produtos_fornecedores)["total_produtos_fornecedores"].values[0]
 
     def get_total_produtos(self):
-        # Cria uma nova conexão com o banco que permite alteração
         oracle = ConexaoOracle()
         oracle.connect()
-        # Retorna o total de registros computado pela query
         return oracle.sqlToDataFrame(self.qry_total_produtos)["total_produtos"].values[0]
 
     def get_updated_screen(self):
-        return f"""
+        largura = 70
+        borda_linha = "#" * largura
 
-
-
+        criadores = [
+            "Arthur Pomarolli",
+            "Davi de Souza",
+            "Mauro Barros",
+            "Pedro Augusto"
+        ]
+        professor = "Prof. M.Sc. Howard Roatti."
+        disciplina = "Banco de dados"
+        semestre = "2025/2"
         
-                                ########################################################
-                                #                   SISTEMA DE VENDAS                               
-                                #                                                        
-                                #  TOTAL DE REGISTROS:                                   
-                                #      1 - FORNECEDORES:       {str(self.get_total_fornecedores()).rjust(5)}
-                                #      2 - FUNCIONARIOS:       {str(self.get_total_funcionarios()).rjust(5)}
-                                #      3 - MOV. ESTOQUE:       {str(self.get_total_movimentacoes_estoque()).rjust(5)}
-                                #      4 - PROD. FORNECEDORES: {str(self.get_total_produtos_fornecedores()).rjust(5)}
-                                #      5 - PRODUTOS:           {str(self.get_total_produtos()).rjust(5)}
-                                #
-                                # DADOS DO GRUPO:
-                                #
-                                #  CRIADO POR: {self.created_by}
-                                #
-                                #  PROFESSOR: {self.professor}
-                                #
-                                #  DISCIPLINA: {self.disciplina}
-                                #              {self.semestre}
-                                ########################################################
-        """
+        def format_line(content=""):
+            return f"#{content.ljust(largura - 2)}#"
+        
+        criadores_str = ""
+        for i, nome in enumerate(criadores):
+            prefixo = "  CRIADO POR: " if i == 0 else "               "
+            criadores_str += format_line(f"{prefixo}{nome}") + "\n"
+        criadores_str = criadores_str.rstrip("\n")
+
+        return f"""
+{borda_linha}
+{format_line("SISTEMA DE VENDAS".center(largura - 2))}
+{format_line()}
+{format_line("   TOTAL DE REGISTROS:")}
+{format_line(f"   1 - FORNECEDORES:".ljust(30) + str(self.get_total_fornecedores()).rjust(5))}
+{format_line(f"   2 - FUNCIONARIOS:".ljust(30) + str(self.get_total_funcionarios()).rjust(5))}
+{format_line(f"   3 - MOV. ESTOQUE:".ljust(30) + str(self.get_total_movimentacoes_estoque()).rjust(5))}
+{format_line(f"   4 - PROD. FORNECEDORES:".ljust(30) + str(self.get_total_produtos_fornecedores()).rjust(5))}
+{format_line(f"   5 - PRODUTOS:".ljust(30) + str(self.get_total_produtos()).rjust(5))}
+{format_line()}
+{format_line("   DADOS DO GRUPO:")}
+{format_line()}
+{criadores_str}
+{format_line()}
+{format_line(f"   PROFESSOR:".ljust(15) + professor)}
+{format_line()}
+{format_line(f"   DISCIPLINA:".ljust(15) + disciplina)}
+{format_line(f"   SEMESTRE:".ljust(15) + semestre)}
+{borda_linha}
+    """
